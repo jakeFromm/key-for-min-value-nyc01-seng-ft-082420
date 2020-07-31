700be94bbdfb09eb918e4c4460b956a51876e559
Jacob Fromm <jacobfromm@gmail.com>
2020-07-31 03:30:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
-
+  is_low = 0
+  min = ""
+  name_hash.each do |key, value|
+   
+    if 
+      
+    end  
 end

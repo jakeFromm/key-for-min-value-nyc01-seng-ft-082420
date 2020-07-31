f00ffae634b179b479098b4ab5f5aa0ca81da54e
Jacob Fromm <jacobfromm@gmail.com>
2020-07-31 03:40:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |k, v|
      if lowest_value == nil || v < lowest_value
        lowest_value = v 
-       
-       
+       lowest_key = k 
      end    
   end  
 end
